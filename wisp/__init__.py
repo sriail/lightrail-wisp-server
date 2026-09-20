f48245c57aff6c6fe53eca57cdf609c5ef5cca86
Mac <mackinleymorrone@gmail.com>
2026-09-20 17:21:12 -0400

    Delete wisp directory

--- wisp/__init__.py
+++
@@ -1,3 +0,0 @@
-from . import util
-
-version = util.get_version()
--- wisp/util.py
+++
@@ -1,7 +0,0 @@
-import importlib.metadata
-
-def get_version():
-  try:
-    return importlib.metadata.version("wisp-python")
-  except TypeError:
-    return "0.0.0"
